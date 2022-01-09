b6dbaa4dd88d46c6df2d1656da42f6b94c17343b
theNAFIZ <nrpustcse@gmail.com>
2022-01-09 19:09:16 +0600

    Image read write

---
+++ basics.py
@@ -0,0 +1,17 @@
+import cv2
+
+cola_color = cv2.imread("images\coca-cola-logo.png", 1)
+cola_gray = cv2.imread("images\coca-cola-logo.png", 0)
+cola_uncng = cv2.imread("images\coca-cola-logo.png", -1)
+
+cv2.imshow("Color", cola_color)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow("Gray", cola_gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow("Unchanged", cola_uncng)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
